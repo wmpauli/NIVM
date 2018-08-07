08daa60b8915e7fdf3a641732236e48f80c9b3bd
Wolfgang Pauli <wolfgang.pauli@microsoft.com>
2018-08-07 13:33:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,24 +11,24 @@
 # sudo mv apt-get /usr/local/sbin/
 
 # add neurodebian repository
-wget -O- http://neuro.debian.net/lists/xenial.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+wget -O- http://neuro.debian.net/lists/xenial.us-nh.full | tee /etc/apt/sources.list.d/neurodebian.sources.list
 
 # add certificate keys
-sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
 
 # update package cache
-sudo apt-get update
+apt-get update
 
 # install packages
-sudo apt-get install -y afni connectome-workbench connectomeviewer fsl-core fsleyes fsl-harvard-oxford-atlases itksnap 
+apt-get install -y afni connectome-workbench connectomeviewer fsl-core fsleyes fsl-harvard-oxford-atlases itksnap 
 
 # remove obsolete packages
-sudo apt-get -y autoremove
+apt-get -y autoremove
 
 # configure FSL
 cd /tmp/
 wget https://raw.githubusercontent.com/wmpauli/NIVM/master/fsl_configure.sh
-sudo mv fsl_configure.sh /etc/profile.d/
+mv fsl_configure.sh /etc/profile.d/
 
 # ensure that these settings also work in non-login shells
-echo "source /etc/profile.d/fsl_configure.sh" | sudo tee --append /etc/bash.bashrc
+echo "source /etc/profile.d/fsl_configure.sh" | tee --append /etc/bash.bashrc
