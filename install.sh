7c9dfa860386f3bd7e041cfa15a53ed32c432b47
Wolfgang Pauli <wolfgang.pauli@microsoft.com>
2018-08-06 21:00:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ sudo apt-get update
 # the following packages appear to be unavailable on neurodebian.org (for ubuntu 16.04): caret, *openmeeg*, pydicom
 # we are also not installing spm, because that would require prior installation of matlab by the user
 # we are installing fsleyes instead of fslview
-sudo apt-get install -y afni afni-common afni-dbg afni-dev connectome-workbench connectome-workbench-dbg connectomeviewer dcm2niix python-dcmstack debruijn dicomnifti fsl-core fsleyes fslview-doc heudiconv itksnap mriconvert mricron mricron-data mricron-doc mridefacer python-nibabel python-nibabel-doc python3-nibabel libnifti-dev libnifti-doc libnifti2 nifti-bin python-nipy python-nipy-doc python-nipy-lib python-nipy-lib-dbg python-nipype python-nipype-doc python-nitime python-nitime-doc python-dicom python3-dicom python-mvpa2 python-mvpa2-doc python-mvpa2-lib python-nifti python-pyxnat python-statsmodels python-statsmodels-doc python-statsmodels-lib voxbo fsl-harvard-oxford-atlases
+sudo apt-get install -y afni # afni-common afni-dbg afni-dev connectome-workbench connectome-workbench-dbg connectomeviewer dcm2niix python-dcmstack debruijn dicomnifti fsl-core fsleyes fslview-doc heudiconv itksnap mriconvert mricron mricron-data mricron-doc mridefacer python-nibabel python-nibabel-doc python3-nibabel libnifti-dev libnifti-doc libnifti2 nifti-bin python-nipy python-nipy-doc python-nipy-lib python-nipy-lib-dbg python-nipype python-nipype-doc python-nitime python-nitime-doc python-dicom python3-dicom python-mvpa2 python-mvpa2-doc python-mvpa2-lib python-nifti python-pyxnat python-statsmodels python-statsmodels-doc python-statsmodels-lib voxbo fsl-harvard-oxford-atlases
 
 # remove obsolete packages
 sudo apt-get autoremove
